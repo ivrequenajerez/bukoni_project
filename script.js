eb4cb06a3a0791397247d953452ad57b6b4cf671
ivrequenajerez <ianrequenajerez@gmail.com>
2023-05-07 09:32:23 +0200

    Dropdown editado, header editado

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 /* Header */
 
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-    } else {
-    header.classList.remove("sticky");
-    }
-}
-
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
 
 /* FIN Header */
